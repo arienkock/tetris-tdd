2540a1aa5aed0cee5c0da7d565102c550614c60a
Arien Kock <akock@worth.systems>
2020-09-13 08:30:03 +0200

    Add test for shape at top of area

--- spec/game.spec.js
+++ spec/game.spec.js
@@ -5,10 +5,29 @@ describe("tetris", () => {
     expect(dimensions.height).toBeGreaterThan(1);
     expect(dimensions.width).toBeGreaterThan(1);
   });
+
+  it("has a shape at the top and center when the game starts", () => {
+    const game = new Tetris();
+    const shape = game.getCurrentShape();
+    const { width } = game.getAreaDimensions();
+    expect(shape.y).toBe(0);
+    expect(shape.x).toBe(Math.floor(width / 2 - shape.width / 2));
+  });
 });
 
 function Tetris() {
-  const height = 40;
-  const width = 10;
-  this.getAreaDimensions = () => ({ height, width });
+  const area = {
+    width: 10,
+    height: 40,
+  };
+  this.getAreaDimensions = () => area;
+  this.getCurrentShape = () => {
+    const width = 4;
+    const x = Math.floor(area.width / 2 - width / 2);
+    return {
+      y: 0,
+      x,
+      width,
+    };
+  };
 }
