d36932f3ee17410edd062c4ae7046a8467ecc61b
Arien Kock <akock@worth.systems>
2020-10-04 21:28:00 +0200

    Show preview in browser

--- src/browser.js
+++ src/browser.js
@@ -14,16 +14,22 @@ const colorPalette = [
   "Gold",
 ];
 
-let rows = "";
-for (let y = 0; y < height; y++) {
-  let columns = "";
-  for (let x = 0; x < width; x++) {
-    columns += `<td class="block block-${x}-${y}"></td>`;
+function generateTableGrid(width, height) {
+  let rows = "";
+  for (let y = 0; y < height; y++) {
+    let columns = "";
+    for (let x = 0; x < width; x++) {
+      columns += `<td class="block block-${x}-${y}"></td>`;
+    }
+    rows += "<tr>" + columns + "</tr>";
   }
-  rows += "<tr>" + columns + "</tr>";
+  return rows;
 }
 
-const table = "<table class='area'>" + rows + "</table>";
+const playArea =
+  '<table class="area">' + generateTableGrid(width, height) + "</table>";
+const previewArea =
+  '<table class="preview-area">' + generateTableGrid(4, 4) + "</table>";
 const status =
   'Level:<span class="level"></span> Score:<span class="score"></span>';
 
@@ -31,7 +37,7 @@ let html = `
 <div class="status-and-area">
   <div class="status">${status}</div>
   <div class="area-container">
-    ${table}
+    <div class="area-and-preview">${playArea}${previewArea}</div>
     <div class='game-over hidden'><h1 class='game-over-label'>Game Over</h1><div>
   </div>
 </div>`;
@@ -51,11 +57,19 @@ function paint() {
   const piece = game.getPiece();
   for (let y = 0; y < height; y++) {
     for (let x = 0; x < width; x++) {
-      setActive(x, y, !!game.getAreaContents(x, y));
+      setActive(".area", x, y, !!game.getAreaContents(x, y));
     }
   }
   piece.shape.blocks.forEach(([x, y]) => {
-    setActive(x + piece.x, y + piece.y, true);
+    setActive(".area", x + piece.x, y + piece.y, true);
+  });
+  for (let y = 0; y < 4; y++) {
+    for (let x = 0; x < 4; x++) {
+      setActive(".preview-area", x, y, false);
+    }
+  }
+  game.nextShape.blocks.forEach(([x, y]) => {
+    setActive(".preview-area", x, y, true);
   });
   levelEl.textContent = game.score.level.toString().padStart(3, "0").slice(-3);
   scoreEl.textContent = game.score.value.toString().padStart(7, "0").slice(-7);
@@ -66,8 +80,8 @@ function paint() {
     colorPalette[game.score.level % colorPalette.length];
 }
 
-function setActive(x, y, isActive) {
-  const td = document.querySelector(`.block-${x}-${y}`);
+function setActive(scopeSelector, x, y, isActive) {
+  const td = document.querySelector(`${scopeSelector} .block-${x}-${y}`);
   if (!td) {
     console.error("No block for", `.block-${x}-${y}`);
   } else if (isActive) {
